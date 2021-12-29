365a619b28e312b2f265826e4fc63ec4877e2e35
Luiz Fellipe Carneiro <luiz@fellipec.com>
2021-12-28 23:53:38 -0300

    Now tweets images!

--- tweetrndrss.py
+++ tweetrndrss.py
@@ -4,6 +4,7 @@ import feedparser
 import random
 import sqlite3
 import ssl
+import requests
 
 #Contorno para erros do SSL com o feedparser
 if hasattr(ssl, '_create_unverified_context'):
@@ -26,7 +27,8 @@ if cur.fetchone()[0]==0 : #Tabela RSS não existe, cria-se a tabela
     conn.execute('''CREATE TABLE RSS (
     ID    CHAR(50) PRIMARY KEY NOT NULL,
     TITLE TEXT                 NOT NULL,
-    LINK  TEXT);''')
+    LINK  TEXT,
+    MEDIA TEXT);''')
     print("Tabela RSS Criada")
     conn.commit()
 else :
@@ -34,7 +36,8 @@ else :
 
 #Insere o RSS no banco de dados
 for entry in NewsFeed.entries:
-    cur.execute("INSERT OR IGNORE INTO RSS VALUES (?, ?, ?)",(entry.id,entry.title,entry.link))
+    if entry.has_key('media_thumbnail') :
+        cur.execute("INSERT OR IGNORE INTO RSS VALUES (?, ?, ?, ?)",(entry.id,entry.title,entry.link,entry.media_thumbnail[0]['url']))
     conn.commit()
 
 #Recupera o total de artigos salvos no banco de dados e sorteia um
@@ -43,20 +46,28 @@ total = int(cur.fetchone()[0])
 rndArticleID = random.randint(1,total)
 
 #Recupera a entrada com o ID aleatório escolhido
-cur.execute('SELECT LINK, TITLE FROM RSS WHERE ROWID = ?',(rndArticleID,))
+cur.execute('SELECT LINK, TITLE, MEDIA FROM RSS WHERE ROWID = ?',(rndArticleID,))
 article = cur.fetchone()
 
 #Gera um Tuite com o título e URL do artigo
-TweetText = "Scarlett! " #+ article[1]
-TweetText = TweetText[0:270-len(article[0])] + " " + article[0]
+TweetText = "Scarlett! " + article[1]
+TweetText = TweetText[0:200-len(article[0])] + " " + article[0]
+r = requests.get(article[2])
+pic = open('scarlett.jpg','wb')
+pic.write(r.content)
+pic.close
+
 print(TweetText)
+
+
 
 #Configura o Twitter
 authtw = tweepy.OAuthHandler(consumer_key(),consumer_secret())
 authtw.set_access_token(access_token_key(),access_token_secret())
 api = tweepy.API(authtw,wait_on_rate_limit=True)
 
-api.update_status(TweetText)
+api.update_status_with_media(TweetText,'scarlett.jpg')
+#api.update_status(TweetText)
 
 #Fecha o banco de dados
 conn.commit()
