98569e6c414113430fc746c9cf706bcd2462a76b
Luiz Fellipe Carneiro <luiz@fellipec.com>
2021-12-29 09:50:02 -0300

    Refactor and add English comments

--- tweetrndrss.py
+++ tweetrndrss.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from feedparser.util import FeedParserDict
 import tweepy
 import feedparser
 import random
@@ -7,23 +8,33 @@ import ssl
 import requests
 
 #Contorno para erros do SSL com o feedparser
+#Workarround to SSL errors with feedparser
 if hasattr(ssl, '_create_unverified_context'):
     ssl._create_default_https_context = ssl._create_unverified_context
 
 #Informações de autenticação estão no arquivo auth.py
 #Edite o arquivo sample_auth.py com as credenciais corretas e salve como auth.py
+#Authentication information is on auth.py file. 
+#Edit the file sample_auth.py with your correct credentials and save as auth.py
 from auth import *
 
-#Definições
-NewsFeed = feedparser.parse("https://www.reddit.com/r/ScarlettJohansson/.rss")
+#Definições globais
+#Global definitions
+FeedURL = "https://www.reddit.com/r/ScarlettJohansson/.rss"
+TweetMessage = 'Scarlett! \n'
+TweetSize = 200
+useImage = True
+imageFile = 'scarlett.jpg'
 
 #Inicialização do Banco de Dados
+#Database init
 conn = sqlite3.connect('rss.db')
 cur = conn.cursor()
 
 #Verifica se existe a tabela FEED
+#Checks for the FEED table
 cur.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='RSS' ''')
-if cur.fetchone()[0]==0 : #Tabela RSS não existe, cria-se a tabela
+if cur.fetchone()[0]==0 : #Tabela RSS não existe, cria-se a tabela / Table don't exists, creat it
     conn.execute('''CREATE TABLE RSS (
     ID    CHAR(50) PRIMARY KEY NOT NULL,
     TITLE TEXT                 NOT NULL,
@@ -35,39 +46,50 @@ else :
     print("Tabela RSS já existe")
 
 #Insere o RSS no banco de dados
+#INSERT the RSS feed into the database
+NewsFeed = feedparser.parse(FeedURL)
 for entry in NewsFeed.entries:
+    mediaurl = ''
     if entry.has_key('media_thumbnail') :
-        cur.execute("INSERT OR IGNORE INTO RSS VALUES (?, ?, ?, ?)",(entry.id,entry.title,entry.link,entry.media_thumbnail[0]['url']))
+        mediaurl = entry.media_thumbnail[0]['url']
+    cur.execute("INSERT OR IGNORE INTO RSS VALUES (?, ?, ?, ?)",(entry.id,entry.title,entry.link,mediaurl))
     conn.commit()
 
 #Recupera o total de artigos salvos no banco de dados e sorteia um
+#Finds the total of articles in the database and select a random one
 cur.execute('SELECT COUNT(*) FROM RSS')
 total = int(cur.fetchone()[0])
 rndArticleID = random.randint(1,total)
 
 #Recupera a entrada com o ID aleatório escolhido
+#SELECTs the random article
 cur.execute('SELECT LINK, TITLE, MEDIA FROM RSS WHERE ROWID = ?',(rndArticleID,))
 article = cur.fetchone()
 
 #Gera um Tuite com o título e URL do artigo
-TweetText = "Scarlett! \n" + article[1]
-TweetText = TweetText[0:200-len(article[0])] + " " + article[0]
-r = requests.get(article[2])
-pic = open('scarlett.jpg','wb')
-pic.write(r.content)
-pic.close
-
+#Generates the Tweet
+TweetText = TweetMessage + article[1]
+TweetText = TweetText[0:TweetSize-len(article[0])] + " " + article[0]
 print(TweetText)
 
-
-
-#Configura o Twitter
+#Configura a API do Twitter
+#Configure the Twitter API
 authtw = tweepy.OAuthHandler(consumer_key(),consumer_secret())
 authtw.set_access_token(access_token_key(),access_token_secret())
 api = tweepy.API(authtw,wait_on_rate_limit=True)
 
-api.update_status_with_media(TweetText,'scarlett.jpg')
-#api.update_status(TweetText)
+if useImage:
+    #Baixa a imagem do feed RSS
+    #Download the image from RSS feed
+    r = requests.get(article[2])
+    pic = open(imageFile,'wb')
+    pic.write(r.content)
+    pic.close
+    api.update_status_with_media(TweetText,imageFile)
+else:
+    #Apenas tuita um texto
+    #Just tweet the text
+    api.update_status(TweetText)
 
 #Fecha o banco de dados
 conn.commit()
